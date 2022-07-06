645d1591683e045fedd4a3cb967192340a19f65a
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2022-07-06 20:45:00 +0200

    feat: add default body style

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,16 @@
+import clsx from "clsx";
 import { Head, Html, Main, NextScript } from "next/document";
 
 const Document = () => {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body
+        className={clsx(
+          "bg-white text-black",
+          "dark:bg-neutral-800 dark:text-neutral-100"
+        )}
+      >
         <Main />
         <NextScript />
       </body>
