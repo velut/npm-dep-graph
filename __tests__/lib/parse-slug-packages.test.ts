cae61df9fad0ab4878a8d2e5ce701717639355b0
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2022-09-02 19:07:58 +0200

    feat: update parseSlugPackages and add tests

---
+++ __tests__/lib/parse-slug-packages.test.ts
@@ -0,0 +1,168 @@
+import parseSlugPackages from "../../lib/parse-slug-packages";
+
+const slug = (s: string): string[] => {
+  return s.split("/");
+};
+
+describe("parseSlugPackages", () => {
+  it("empty array", () => {
+    expect(parseSlugPackages([])).toEqual([]);
+  });
+  it("empty slug", () => {
+    expect(parseSlugPackages(slug(""))).toEqual([]);
+  });
+  it("wrong slugs", () => {
+    expect(parseSlugPackages(slug(" "))).toEqual([]);
+    expect(parseSlugPackages(slug("/"))).toEqual([]);
+    expect(parseSlugPackages(slug("///"))).toEqual([]);
+    expect(parseSlugPackages(slug("@aaa/@aaa"))).toEqual([]);
+    expect(parseSlugPackages(slug("@aaa/@aaa,bbb"))).toEqual(["@aaa/bbb"]);
+    expect(parseSlugPackages(slug("aaa/aaa"))).toEqual(["aaa"]);
+    expect(parseSlugPackages(slug("aaa/aaa@1.0.0/bbb"))).toEqual([
+      "aaa",
+      "aaa@1.0.0",
+      "bbb",
+    ]);
+    expect(parseSlugPackages(slug("aaa/bbb"))).toEqual(["aaa", "bbb"]);
+    expect(parseSlugPackages(slug("aaa/@bbb"))).toEqual(["aaa"]);
+    expect(parseSlugPackages(slug("aaa/@bbb/ccc"))).toEqual([
+      "@bbb/ccc",
+      "aaa",
+    ]);
+  });
+  it("bare package", () => {
+    expect(parseSlugPackages(slug("foobar"))).toEqual(["foobar"]);
+  });
+  it("bare package with version", () => {
+    expect(parseSlugPackages(slug("foobar@1.0.0"))).toEqual(["foobar@1.0.0"]);
+  });
+  it("scoped package", () => {
+    expect(parseSlugPackages(slug("@foo/bar"))).toEqual(["@foo/bar"]);
+  });
+  it("scoped package with version", () => {
+    expect(parseSlugPackages(slug("@foo/bar@1.0.0"))).toEqual([
+      "@foo/bar@1.0.0",
+    ]);
+  });
+  it("multiple bare packages", () => {
+    expect(
+      parseSlugPackages(slug("aaa,bbb,ccc,ddd.js,eee-ts,fff_v2,2022,123go"))
+    ).toEqual([
+      "123go",
+      "2022",
+      "aaa",
+      "bbb",
+      "ccc",
+      "ddd.js",
+      "eee-ts",
+      "fff_v2",
+    ]);
+  });
+  it("multiple bare packages with spacing", () => {
+    expect(
+      parseSlugPackages(slug("  aaa, bbb, ccc ,  ddd  ,eee,fff  "))
+    ).toEqual(["aaa", "bbb", "ccc", "ddd", "eee", "fff"]);
+  });
+  it("multiple bare packages with version", () => {
+    expect(parseSlugPackages(slug("aaa@1.0.0,bbb@2.0.0,ccc@3.0.0"))).toEqual([
+      "aaa@1.0.0",
+      "bbb@2.0.0",
+      "ccc@3.0.0",
+    ]);
+  });
+  it("multiple bare packages, some with version", () => {
+    expect(parseSlugPackages(slug("aaa@1.0.0,bbb,ccc@3.0.0,ddd"))).toEqual([
+      "aaa@1.0.0",
+      "bbb",
+      "ccc@3.0.0",
+      "ddd",
+    ]);
+  });
+  it("multiple bare packages with spacing, some with version", () => {
+    expect(
+      parseSlugPackages(slug("aaa@1.0.0 , bbb  ,  ccc@3.0.0, ddd"))
+    ).toEqual(["aaa@1.0.0", "bbb", "ccc@3.0.0", "ddd"]);
+  });
+  it("multiple scoped packages", () => {
+    expect(
+      parseSlugPackages(
+        slug(
+          "@foo/aaa,@foo/bbb,@foo/ccc,@foo/ddd.js,@foo/eee-ts,@foo/fff_v2,@foo/123go,@foo/2022"
+        )
+      )
+    ).toEqual([
+      "@foo/123go",
+      "@foo/2022",
+      "@foo/aaa",
+      "@foo/bbb",
+      "@foo/ccc",
+      "@foo/ddd.js",
+      "@foo/eee-ts",
+      "@foo/fff_v2",
+    ]);
+  });
+  it("multiple scoped packages with spacing", () => {
+    expect(
+      parseSlugPackages(slug("  @foo/aaa, @foo/bbb  ,  @foo/ccc  "))
+    ).toEqual(["@foo/aaa", "@foo/bbb", "@foo/ccc"]);
+  });
+  it("multiple scoped packages with version", () => {
+    expect(
+      parseSlugPackages(slug("@foo/aaa@1.0.0,@foo/bbb@2.0.0,@foo/ccc@3.0.0"))
+    ).toEqual(["@foo/aaa@1.0.0", "@foo/bbb@2.0.0", "@foo/ccc@3.0.0"]);
+  });
+  it("multiple scoped packages, some with version", () => {
+    expect(
+      parseSlugPackages(slug("@foo/aaa@1.0.0,@foo/bbb,@foo/ccc@3.0.0,@foo/ddd"))
+    ).toEqual(["@foo/aaa@1.0.0", "@foo/bbb", "@foo/ccc@3.0.0", "@foo/ddd"]);
+  });
+  it("multiple scoped packages with spacing, some with version", () => {
+    expect(
+      parseSlugPackages(
+        slug("  @foo/aaa@1.0.0 , @foo/bbb,   @foo/ccc@3.0.0,@foo/ddd  ")
+      )
+    ).toEqual(["@foo/aaa@1.0.0", "@foo/bbb", "@foo/ccc@3.0.0", "@foo/ddd"]);
+  });
+  it("mixed bare and scoped packages", () => {
+    expect(parseSlugPackages(slug("aaa,@foo/aaa,bbb,@foo/bbb"))).toEqual([
+      "@foo/aaa",
+      "@foo/bbb",
+      "aaa",
+      "bbb",
+    ]);
+    expect(parseSlugPackages(slug("@foo/aaa,bbb,@foo/bbb,aaa"))).toEqual([
+      "@foo/aaa",
+      "@foo/bbb",
+      "aaa",
+      "bbb",
+    ]);
+    expect(
+      parseSlugPackages(slug(" aaa , bbb   ,  @foo/aaa,@foo/bbb  "))
+    ).toEqual(["@foo/aaa", "@foo/bbb", "aaa", "bbb"]);
+  });
+  it("mixed bare and scoped packages, some with version", () => {
+    expect(
+      parseSlugPackages(slug("aaa@1.0.0,@foo/aaa,bbb,@foo/bbb@2.0.0"))
+    ).toEqual(["@foo/aaa", "@foo/bbb@2.0.0", "aaa@1.0.0", "bbb"]);
+    expect(
+      parseSlugPackages(slug("@foo/aaa,bbb,@foo/bbb@2.0.0,aaa@1.0.0"))
+    ).toEqual(["@foo/aaa", "@foo/bbb@2.0.0", "aaa@1.0.0", "bbb"]);
+    expect(
+      parseSlugPackages(slug(" aaa@1.0.0 , bbb   ,  @foo/aaa,@foo/bbb@2.0.0  "))
+    ).toEqual(["@foo/aaa", "@foo/bbb@2.0.0", "aaa@1.0.0", "bbb"]);
+  });
+  it("identical packages, some with version", () => {
+    expect(
+      parseSlugPackages(
+        slug("aaa,@foo/aaa,bbb@1.0.0,@foo/bbb@2.0.0,aaa,@foo/aaa,ccc,ccc@3.0.0")
+      )
+    ).toEqual([
+      "@foo/aaa",
+      "@foo/bbb@2.0.0",
+      "aaa",
+      "bbb@1.0.0",
+      "ccc",
+      "ccc@3.0.0",
+    ]);
+  });
+});
--- lib/parse-slug-packages.ts
+++ lib/parse-slug-packages.ts
@@ -1,31 +1,36 @@
-const parseSlugPackages = (slug: string[]) => {
-  return (
-    slug
-      // The slug contains packages already split by `/`,
-      // this happens when scoped packages like `@foo/bar` are present.
-      // Now we also split by `,`, which is used to separate multiple packages,
-      // for example `@foo/bar,baz`.
-      // The result is a list of either bare names (`baz`), scopes (`@foo`)
-      // or scoped names (`bar`).
-      .flatMap((part) => part.split(","))
-      // Rebuild package names from their parts.
-      .flatMap((part, index, parts) => {
-        // Ignore scopes, we add them later with the name.
-        if (part.startsWith("@")) {
-          return [];
-        }
-        // If this bare name (`bar`) was preceded by a scope (`@foo`),
-        // then return the scoped package name (`@foo/bar`).
-        const prevPart = parts[index - 1];
-        const isScopedName = prevPart?.startsWith("@") ?? false;
-        if (isScopedName) {
-          return `${prevPart}/${part}`;
-        }
-        // Return the normal package name (`baz`).
-        return part;
-      })
-      .sort((a, b) => a.localeCompare(b))
-  );
+/**
+ * @param slug the slug list parsed by Next.js
+ * @returns a sorted list of unique package names and/or package version IDs
+ */
+const parseSlugPackages = (slug: string[]): string[] => {
+  return Array.from(
+    new Set(
+      slug
+        // The slug contains packages already split by `/`
+        // if scoped packages like `@foo/bar` are present.
+        // We also split by `,`, which is used to separate multiple packages.
+        // For example, `aaa,@foo/bbb` becomes `['aaa', '@foo', 'bbb']`,
+        // resulting in a list of either bare names (`aaa`), scopes (`@foo`)
+        // or scoped names (`bbb`).
+        .flatMap((part) => part.split(","))
+        // Rebuild package names from their parts.
+        .flatMap((rawPart, index, parts) => {
+          const part = rawPart.trim();
+          // Ignore empty parts or scopes (`@foo`), which we add below.
+          if (part === "" || part.startsWith("@")) {
+            return [];
+          }
+          // If this bare name (`bbb`) is preceded by a scope (`@foo`),
+          // then return the scoped package name (`@foo/bbb`).
+          const prevPart = parts[index - 1]?.trim() ?? "";
+          if (prevPart.startsWith("@")) {
+            return `${prevPart}/${part}`;
+          }
+          // Return the bare package name (`aaa`).
+          return part;
+        })
+    )
+  ).sort((a, b) => a.localeCompare(b));
 };
 
 export default parseSlugPackages;
