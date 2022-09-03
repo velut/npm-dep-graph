c4cb2f08d65d5da677e1617e6e85208800ef0803
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2022-09-03 02:40:21 +0200

    feat: update resolvePackages

--- lib/resolve-packages.ts
+++ lib/resolve-packages.ts
@@ -1,21 +1,20 @@
 import pMap, { pMapSkip } from "p-map";
+import fetchPackageManifest from "./fetch-package-manifest";
 
-const resolvePackageId = async (pkg: string): Promise<string> => {
+const resolvePackage = async (pkg: string): Promise<string> => {
   const versionPrefixPos = pkg.lastIndexOf("@");
   const hasVersion = versionPrefixPos > 0;
   const name = hasVersion ? pkg.slice(0, versionPrefixPos) : pkg;
-  const version = hasVersion
-    ? pkg.slice(versionPrefixPos + 1)
-    : // TODO: query npm registry
-      await Promise.resolve("TODO");
-  return `${name}@${version}`;
+  const version = hasVersion ? pkg.slice(versionPrefixPos + 1) : undefined;
+  const { id } = await fetchPackageManifest(name, version);
+  return id;
 };
 
-const tryResolvePackageId = async (
+const tryResolvePackage = async (
   pkg: string
 ): Promise<string | typeof pMapSkip> => {
   try {
-    const id = await resolvePackageId(pkg);
+    const id = await resolvePackage(pkg);
     return id;
   } catch {
     return pMapSkip;
@@ -23,7 +22,7 @@ const tryResolvePackageId = async (
 };
 
 const resolvePackages = async (packages: string[]): Promise<string[]> => {
-  return pMap(packages, tryResolvePackageId, { concurrency: 2 });
+  return pMap(packages, tryResolvePackage, { concurrency: 2 });
 };
 
 export default resolvePackages;
