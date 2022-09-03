fc747e532a11bf36e3c025dfafb70222017c6e1d
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2022-09-03 21:41:25 +0200

    feat: click nodes in the diagram to go to the registry page

--- lib/build-diagram.ts
+++ lib/build-diagram.ts
@@ -1,7 +1,22 @@
+import urlJoin from "url-join";
+import packageFromId from "./package-from-id";
+import registryWeb from "./registry-web";
+
+// See https://mermaid-js.github.io/mermaid/#/flowchart
 const buildDiagram = (packages: string[]): string => {
-  const nodes = packages.map((pkg, index) => `p${index}("${pkg}")`);
+  const nodes = packages.map((pkg, index) => {
+    // Create nodes like `p0("foo")` where `p0` is the ID,
+    // the parentheses `()` render rounded box corners
+    // and the quotes `""` escape the package name.
+    return `p${index}("${pkg}")`;
+  });
   const edges: string[] = [];
-  const interactions: string[] = [];
+  const interactions = packages.map((pkg, index) => {
+    // Make nodes clickable and point them to their registry webpage.
+    const { name, version } = packageFromId(pkg);
+    const url = urlJoin(registryWeb, "package", name, "v", version ?? "latest");
+    return `click p${index} "${url}"`;
+  });
   return ["graph TB", ...nodes, ...edges, ...interactions].join("\n");
 };
 
