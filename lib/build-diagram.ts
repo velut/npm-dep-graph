c76e73babd2e8fc8adbafb9cd0602f205b77d62d
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2022-09-04 18:37:36 +0200

    refactor: refactor buildDiagram

--- lib/build-diagram.ts
+++ lib/build-diagram.ts
@@ -2,22 +2,34 @@ import urlJoin from "url-join";
 import packageFromId from "./package-from-id";
 import registryWeb from "./registry-web";
 
-// See https://mermaid-js.github.io/mermaid/#/flowchart
-const buildDiagram = (packages: string[]): string => {
-  const nodes = packages.map((pkg, index) => {
-    // Create nodes like `p0("foo")` where `p0` is the ID,
-    // the parentheses `()` render rounded box corners
-    // and the quotes `""` escape the package name.
-    return `p${index}("${pkg}")`;
-  });
-  const edges: string[] = [];
-  const interactions = packages.map((pkg, index) => {
-    // Make nodes clickable and point them to their registry webpage.
+const buildNodes = (packages: string[]): string[] => {
+  // Create nodes like `p0("foo@1.0.0")` where `p0` is the node ID,
+  // the parentheses `()` tell mermaid to render rounded box corners
+  // and the quotes `""` contain the package version ID.
+  return packages.map((pkg, index) => `p${index}("${pkg}")`);
+};
+
+const buildEdges = (packages: string[]): string[] => {
+  return [];
+};
+
+const buildInteractions = (packages: string[]): string[] => {
+  // Make nodes clickable and point them to their registry webpage.
+  return packages.map((pkg, index) => {
     const { name, version } = packageFromId(pkg);
     const url = urlJoin(registryWeb, "package", name, "v", version ?? "latest");
     return `click p${index} "${url}"`;
   });
-  return ["graph TB", ...nodes, ...edges, ...interactions].join("\n");
+};
+
+// See https://mermaid-js.github.io/mermaid/#/flowchart
+const buildDiagram = (packages: string[]): string => {
+  return [
+    "graph TB",
+    ...buildNodes(packages),
+    ...buildEdges(packages),
+    ...buildInteractions(packages),
+  ].join("\n");
 };
 
 export default buildDiagram;
