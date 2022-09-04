810c2c1d80d5485d5b5e75445763b7fee6402325
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2022-09-04 18:50:14 +0200

    docs: update docs

--- lib/package-from-id.ts
+++ lib/package-from-id.ts
@@ -1,4 +1,6 @@
 const packageFromId = (id: string): { name: string; version?: string } => {
+  // Find the name and version given a package ID.
+  // For example, `foo@1.0.0` => `{ name: "foo", version: "1.0.0"}`.
   const versionPrefixPos = id.lastIndexOf("@");
   const hasVersion = versionPrefixPos > 0;
   const name = hasVersion ? id.slice(0, versionPrefixPos) : id;
