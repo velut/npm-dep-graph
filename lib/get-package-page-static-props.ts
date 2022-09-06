2bf5ef21a768712f9d58be51fda92fac082f59b0
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2022-09-06 18:32:03 +0200

    refactor: add logging

--- lib/get-package-page-static-props.ts
+++ lib/get-package-page-static-props.ts
@@ -1,5 +1,6 @@
 import { GetStaticPropsResult } from "next";
 import buildDiagram from "./build-diagram";
+import logger from "./logger";
 import { Package } from "./package";
 import parseSlugPackageNames from "./parse-slug-package-names";
 import resolvePackages from "./resolve-packages";
@@ -13,10 +14,12 @@ export interface PackagePageProps {
 const getPackagePageStaticProps = async (
   slug: string[]
 ): Promise<GetStaticPropsResult<PackagePageProps>> => {
+  const log = logger.child({ fn: "getPackagePageStaticProps" });
   const names = parseSlugPackageNames(slug);
   const packages = await resolvePackages(names);
   const originalRoute = slug.join("/");
   const canonicalRoute = packages.map(({ id }) => id).join(",");
+  log.info({ slug, names, packages, originalRoute, canonicalRoute });
   if (originalRoute !== canonicalRoute) {
     return {
       redirect: {
