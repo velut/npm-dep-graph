2d6e8b3da2e7fb0305f2f2028f926b534afb7610
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2022-09-07 22:46:38 +0200

    refactor: update imports

--- lib/fetch-packages.ts
+++ lib/fetch-packages.ts
@@ -1,12 +1,12 @@
-import dedupePackages from "./dedupe-packages";
+import dedupeById from "./dedupe-by-id";
 import fetchPackage from "./fetch-package";
 import { Package } from "./package";
-import sortPackagesById from "./sort-packages-by-id";
+import sortById from "./sort-by-id";
 
 const fetchPackages = async (ids: string[]): Promise<Package[]> => {
   const res = await Promise.all(ids.map((id) => fetchPackage(id)));
   const pkgs = res.filter((pkg): pkg is Package => pkg !== undefined);
-  return sortPackagesById(dedupePackages(pkgs));
+  return sortById(dedupeById(pkgs));
 };
 
 export default fetchPackages;
