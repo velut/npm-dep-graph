431ccf71870a28655ec1b985590346592a560049
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2022-09-24 20:27:08 +0200

    feat: memoize fetchPackageManifest

--- lib/fetch-package-manifest.ts
+++ lib/fetch-package-manifest.ts
@@ -1,17 +1,26 @@
 import fetch from "cross-fetch";
+import ExpiryMap from "expiry-map";
+import pMemoize from "p-memoize";
 import urlJoin from "url-join";
 import validateNpmPackageName from "validate-npm-package-name";
+import logger from "./logger";
 import { PackageManifest, packageManifestSchema } from "./package-manifest";
 import registryApi from "./registry-api";
 
-const fetchPackageManifest = async (
+const log = logger.child({ fn: "fetchPackageManifest" });
+
+const cache = new ExpiryMap(2 * 60 * 1000);
+
+const _fetchPackageManifest = async (
   name: string,
   version = "latest"
 ): Promise<PackageManifest> => {
   if (!validateNpmPackageName(name).validForOldPackages) {
+    log.error({ pkgName: name }, "invalid name for package");
     throw new Error(`fetchPackageManifest: invalid name for package: ${name}`);
   }
   if (version === "") {
+    log.error({ pkgName: name }, "empty version for package");
     throw new Error(`fetchPackageManifest: empty version for package: ${name}`);
   }
   const res = await fetch(urlJoin(registryApi, name, version));
@@ -19,5 +28,10 @@ const fetchPackageManifest = async (
   const manifest = packageManifestSchema.parse(data);
   return manifest;
 };
+
+const fetchPackageManifest = pMemoize(_fetchPackageManifest, {
+  cache,
+  cacheKey: (args) => JSON.stringify(args),
+});
 
 export default fetchPackageManifest;
