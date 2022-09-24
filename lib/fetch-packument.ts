7cb3ea0fb5f49af3a387dd1676246896410d92f5
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2022-09-24 20:19:04 +0200

    feat: memoize fetchPackument

--- lib/fetch-packument.ts
+++ lib/fetch-packument.ts
@@ -1,4 +1,6 @@
 import fetch from "cross-fetch";
+import ExpiryMap from "expiry-map";
+import pMemoize from "p-memoize";
 import urlJoin from "url-join";
 import validateNpmPackageName from "validate-npm-package-name";
 import logger from "./logger";
@@ -7,10 +9,12 @@ import registryApi from "./registry-api";
 
 const log = logger.child({ fn: "fetchPackument" });
 
+const cache = new ExpiryMap(2 * 60 * 1000);
+
 const abbreviatedOrFullPackument =
   "application/vnd.npm.install-v1+json;q=1.0,application/json;q=0.8,*/*";
 
-const fetchPackument = async (name: string): Promise<Packument> => {
+const _fetchPackument = async (name: string): Promise<Packument> => {
   if (!validateNpmPackageName(name).validForOldPackages) {
     log.error({ pkgName: name }, "invalid name for package");
     throw new Error(`fetchPackument: invalid name for package: ${name}`);
@@ -24,5 +28,10 @@ const fetchPackument = async (name: string): Promise<Packument> => {
   const packument = packumentSchema.parse(data);
   return packument;
 };
+
+const fetchPackument = pMemoize(_fetchPackument, {
+  cache,
+  cacheKey: (args) => JSON.stringify(args),
+});
 
 export default fetchPackument;
