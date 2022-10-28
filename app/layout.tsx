d002ba084f80eba948559530c6ca3df82260574f
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2022-10-28 20:08:49 +0200

    refactor: update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <head>
-        <title>Next.js</title>
+        <title>Npm Dependency Graph</title>
         <Script
           id="dark-mode-script"
           strategy="beforeInteractive"
